12fd399d74b10498eff4e4b7122dcd4a3c4a9f21
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-25 13:33:11 +0200

    Refactor swagger route

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addTag('Buddycount')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
