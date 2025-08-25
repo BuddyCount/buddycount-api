2905469041a8e8f9e78a3380b5fbfb5b181459ae
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-25 17:35:31 +0200

    Install typeorm and pg packages + setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [ExpenseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'backend',
+      username: 'backend',
+      entities: [],
+      database: 'backend',
+      synchronize: true,
+      logging: true,
+    }),
+    ExpenseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
