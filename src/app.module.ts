4b105ced59796362f1238e653b2f413a5514ecc9
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-26 16:25:05 +0200

    Working deployment with pgSQL, traefik and adminer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      // host: '127.0.0.1',
+      // host: 'localhost',
+      host: 'db',
       port: 5432,
       password: 'backend',
       username: 'backend',
-      entities: [],
       database: 'backend',
+      entities: [join(__dirname, '**', '*.entity.{js,ts}')],
       synchronize: true,
-      logging: true,
+      // logging: true,
     }),
     ExpenseModule,
   ],
