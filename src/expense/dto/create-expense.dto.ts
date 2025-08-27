a943439b90c6cb26af51cdae0624f4fe510d7291
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-27 12:12:51 +0200

    Fix Expense DTO not showing in swagger

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,8 +1,13 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateExpenseDto {
     @IsString()
     @IsNotEmpty()
-    @Length(3, 30)
+    @Length(1, 30)
+    @ApiProperty({
+        description: 'The name of the expense',
+        example: 'Groceries',
+    })
     name: string;
 }
