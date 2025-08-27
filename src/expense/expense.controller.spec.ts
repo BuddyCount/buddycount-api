4075270ab6f3f3c2569c1749b020c7fe79f70fdb
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-27 09:13:42 +0200

    Fix unit tests by providing the TypeORM Repositories

--- src/expense/expense.controller.spec.ts
+++ src/expense/expense.controller.spec.ts
@@ -1,20 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExpenseController } from './expense.controller';
 import { ExpenseService } from './expense.service';
+import { Expense } from './entities/expense.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ExpenseController', () => {
   let controller: ExpenseController;
+  let service: ExpenseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExpenseController],
-      providers: [ExpenseService],
+      providers: [ExpenseService, {
+        provide: getRepositoryToken(Expense),
+        useValue: {
+          // create: jest.fn(),
+          // save: jest.fn(),
+          findAll: jest.fn(),
+        },
+      }],
     }).compile();
 
     controller = module.get<ExpenseController>(ExpenseController);
+    service = module.get<ExpenseService>(ExpenseService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of expenses', async () => {
+      const result = [new Expense(), new Expense()];
+      jest.spyOn(service, 'findAll').mockImplementation(() => Promise.resolve(result));
+
+      expect(await controller.findAll()).toBe(result);
+    });
   });
 });
--- src/expense/expense.service.spec.ts
+++ src/expense/expense.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExpenseService } from './expense.service';
+import { Expense } from './entities/expense.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ExpenseService', () => {
   let service: ExpenseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExpenseService],
+      providers: [ExpenseService, {
+        provide: getRepositoryToken(Expense),
+        useValue: {
+          // create: jest.fn(),
+          // save: jest.fn(),
+          // findAll: jest.fn(),
+        },
+      }],
     }).compile();
 
     service = module.get<ExpenseService>(ExpenseService);
