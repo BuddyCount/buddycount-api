67ee6d0f51ce3fd0dd89bebf4aef6c3958239567
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-28 20:13:27 +0200

    Fix tests + add two tests examples

--- src/group/group.service.spec.ts
+++ src/group/group.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GroupService } from './group.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Group } from './entities/group.entity';
 
 describe('GroupService', () => {
   let service: GroupService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GroupService],
+      providers: [GroupService, {
+        provide: getRepositoryToken(Group),
+        useValue: {
+          findOne: jest.fn(),
+        },
+      }],
     }).compile();
 
     service = module.get<GroupService>(GroupService);
@@ -14,5 +21,14 @@ describe('GroupService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return a group', async () => {
+      const result = new Group();
+      jest.spyOn(service, 'findOne').mockImplementation(() => Promise.resolve(result));
+
+      expect(await service.findOne('1')).toBe(result);
+    });
   });
 });
