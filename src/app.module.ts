87f68748d1eb578b3f93712f8562f34d90447731
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-29 10:03:20 +0200

    Move data source config to dedicated folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
-import { join } from 'path';
+import { dataSourceOptions } from './config/database/data-source';
 import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      // host: '127.0.0.1',
-      // host: 'localhost',
-      host: 'db',
-      port: 5432,
-      password: 'backend',
-      username: 'backend',
-      database: 'backend',
-      entities: [join(__dirname, '**', '*.entity.{js,ts}')],
-      synchronize: true,
-      // logging: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     ExpenseModule,
     GroupModule,
   ],
