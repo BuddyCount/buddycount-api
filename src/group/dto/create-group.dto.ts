6a384a8c7e155e6f36642fc8a64771104f9b6a2f
Arthur2479 <arthurjacobs1004@gmail.com>
2025-08-30 16:45:27 +0200

    Add basic validators to Group DTO

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,28 +1,42 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsArray, IsEnum, IsNotEmpty, IsString, MaxLength, ValidateNested } from "class-validator";
 import { Currency, UserIndex } from "src/utils/types";
+import { Type } from "class-transformer";
 
 export class CreateGroupDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(30)
     @ApiProperty({
         description: 'The name of the group',
         example: 'Family',
     })
     name: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
     @ApiProperty({
         description: 'The description of the group',
         example: 'Family group',
     })
     description: string;
 
+    @IsNotEmpty()
+    @IsEnum(Currency)
     @ApiProperty({
         description: 'The currency of the group',
         example: Currency.CHF,
     })
     currency: Currency;
 
+    @IsNotEmpty()
+    @IsArray()
+    @Type(() => UserIndex)
+    @ValidateNested({ each: true })
     @ApiProperty({
         description: 'The users of the group',
-        example: [{ id: '1', name: 'John Doe' }],
+        example: [{ id: 1, name: 'John Doe' }],
     })
     users: UserIndex[];
 }
