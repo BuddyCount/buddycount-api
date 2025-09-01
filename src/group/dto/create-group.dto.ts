52625d2f7f415fce1aa73b1707a2c24fe3907a90
Arthur <52378680+Arthur2479@users.noreply.github.com>
2025-09-01 13:56:27 +0200

    Add validators to every dto (#20)
    * Add basic validators to Expense DTO
    
    * Add basic validators to Group DTO
    
    * Turn UserIndex, UserShare and PaidDetails types into DTO : cleaner and allow validation + create RepartitionType Enum, cleaner
    
    * Add second user to new group example to improve manual test efficiency

--- src/group/dto/create-group.dto.ts
+++ src/group/dto/create-group.dto.ts
@@ -1,28 +1,60 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Currency, UserIndex } from "src/utils/types";
+import { IsArray, IsEnum, IsNotEmpty, IsNumber, IsString, MaxLength, ValidateNested } from "class-validator";
+import { Currency } from "src/utils/types";
+import { Type } from "class-transformer";
+
+export class UserIndexDto {
+    @IsNotEmpty()
+    @IsNumber()
+    @ApiProperty({
+        description: 'The id of the user',
+        example: 1,
+    })
+    id: number;
+
+    @IsNotEmpty()
+    @IsString()
+    @ApiProperty({
+        description: 'The name of the user',
+        example: 'John Doe',
+    })
+    name: string;
+}
 
 export class CreateGroupDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(30)
     @ApiProperty({
         description: 'The name of the group',
         example: 'Family',
     })
     name: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
     @ApiProperty({
         description: 'The description of the group',
         example: 'Family group',
     })
     description: string;
 
+    @IsNotEmpty()
+    @IsEnum(Currency)
     @ApiProperty({
         description: 'The currency of the group',
         example: Currency.CHF,
     })
     currency: Currency;
 
+    @IsNotEmpty()
+    @IsArray()
+    @Type(() => UserIndexDto)
+    @ValidateNested({ each: true })
     @ApiProperty({
         description: 'The users of the group',
-        example: [{ id: '1', name: 'John Doe' }],
+        example: [{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }],
     })
-    users: UserIndex[];
+    users: UserIndexDto[];
 }
