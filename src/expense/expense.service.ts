a82f891f504e4018d04cec515fc325775ce650ad
eau2 <94754849+eau2@users.noreply.github.com>
2025-09-01 22:11:36 +0200

    Check paidDetails (#23)
    * Add validation for paidFor amount
    
    * add check sum of portions isn't null
    
    * check share are positive numbers
    
    * sum of share need to be greater or equal to 1, not to 0
    
    * Refactor to remove code duplication and remove portions option for paidby
    
    * Refactor again
    
    * update after rebase
    
    * Formatting and some comments
    
    ---------
    
    Co-authored-by: Arthur2479 <arthurjacobs1004@gmail.com>

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateExpenseDto } from './dto/create-expense.dto';
+import { CreateExpenseDto, UserShareDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
 import { Expense } from './entities/expense.entity';
 import { Repository } from 'typeorm';
@@ -31,22 +31,89 @@ export class ExpenseService {
     }
   }
 
-  private validatePaidByAmount(expenseDto: CreateExpenseDto | UpdateExpenseDto) {
-    if (expenseDto.paidBy?.repartitionType === "AMOUNT") {
-      const total = expenseDto.paidBy.repartition
+  private vaildateAmount(userShare: UserShareDto[], expense: 'paidBy' | 'paidFor', amount?: number) {
+    if (userShare !== undefined && userShare !== null) {
+      const total = userShare
         .map((r: any) => Number(r.values?.amount) || 0)
         .reduce((a, b) => a + b, 0);
-      if (total !== expenseDto.amount) {
+      if (total !== amount) {
         throw new BadRequestException(
-          `Sum of paidBy repartition amounts (${total}) does not match expense amount (${expenseDto.amount})`
+          `Sum of ${expense} repartition amounts (${total}) does not match expense amount (${amount})`
+        );
+      }
+    } else {
+      throw new BadRequestException(
+        `${expense} repartition is required`
+      );
+    }
+  }
+
+  private validatePortions(userShare: UserShareDto[], expense: 'paidBy' | 'paidFor') {
+    if (expense === 'paidBy') {
+      throw new BadRequestException(
+        `Repartition type "PORTIONS" is not allowed for paidBy`
+      );
+    }
+
+    if (userShare === undefined || userShare === null) {
+      throw new BadRequestException(
+        `${expense} repartition is required`
+      );
+    }
+
+    if (expense === "paidFor") {
+      const invalidShares = userShare
+        .map((r: any) => Number(r.values?.share) || 0)
+        .filter((share: number) => share < 0);
+      if (invalidShares.length > 0) {
+        throw new BadRequestException(
+          `Shares in ${expense} repartition must be non-negative numbers`
+        );
+      }
+
+      const totalShares = userShare
+        .map((r: any) => Number(r.values?.share) || 0)
+        .reduce((a, b) => a + b, 0);
+      if (totalShares < 1) {
+        throw new BadRequestException(
+          `Total shares in ${expense} repartition must be greater or equal to one`
         );
       }
     }
   }
 
+  private validatePaid(expenseDto: CreateExpenseDto | UpdateExpenseDto) {
+    // Check paidBy
+    if (expenseDto.paidBy?.repartitionType === "AMOUNT") {
+      this.vaildateAmount(expenseDto.paidBy?.repartition, 'paidBy', expenseDto.amount);
+    } else if (expenseDto.paidBy?.repartitionType === "PORTIONS") {
+      this.validatePortions(expenseDto.paidBy?.repartition, 'paidBy');
+    } else {
+      throw new BadRequestException(
+        `Repartition type "${expenseDto.paidBy?.repartitionType}" is not allowed for paidBy`
+      );
+    }
+
+    // Check paidFor
+    if (expenseDto.paidFor?.repartitionType === "AMOUNT") {
+      this.vaildateAmount(expenseDto.paidFor?.repartition, 'paidFor', expenseDto.amount);
+    } else if (expenseDto.paidFor?.repartitionType === "PORTIONS") {
+      this.validatePortions(expenseDto.paidFor?.repartition, 'paidFor');
+    } else {
+      throw new BadRequestException(
+        `Invalid repartition type "${expenseDto.paidFor?.repartitionType}" for paidFor`
+      );
+    }
+  }
+
   async create(createExpenseDto: CreateExpenseDto) {
-    this.validatePaidByAmount(createExpenseDto);
+    if (!createExpenseDto.groupId) {
+      throw new BadRequestException('groupId is required');
+    }
+
     await this.validateUsersInGroup(createExpenseDto.groupId, this.getConcernedUserIds(createExpenseDto));
+    this.validatePaid(createExpenseDto);
+
     const expense = this.expenseRepository.create(createExpenseDto);
     return this.expenseRepository.save(expense);
   }
@@ -60,11 +127,11 @@ export class ExpenseService {
   }
 
   async update(id: number, updateExpenseDto: UpdateExpenseDto) {
-    this.validatePaidByAmount(updateExpenseDto);
     if (!updateExpenseDto.groupId) {
       throw new BadRequestException('groupId is required');
     }
     await this.validateUsersInGroup(updateExpenseDto.groupId, this.getConcernedUserIds(updateExpenseDto));
+    this.validatePaid(updateExpenseDto);
     return this.expenseRepository.update(id, updateExpenseDto);
   }
 
