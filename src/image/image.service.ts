e947fa0b44b08baa9ac81f0a053a130ae398a83e
Arthur2479 <arthurjacobs1004@gmail.com>
2025-09-03 00:47:09 +0200

    Add function do delete images + refactor image path gen in a func

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -4,24 +4,20 @@ import {
   NotFoundException,
   StreamableFile,
 } from '@nestjs/common';
-import { existsSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 import { join } from 'path';
 import { createReadStream } from 'fs';
 
 @Injectable()
 export class ImageService {
-  constructor() {}
+  constructor() { }
 
   getImage(filename: string): StreamableFile {
     if (!this.isSafeFilename(filename)) {
       throw new BadRequestException('Invalid filename');
     }
 
-    const filePath = join(
-      process.cwd(),
-      process.env.UPLOAD_DIR || 'uploads',
-      filename,
-    );
+    const filePath = this.getFilePath(filename);
 
     if (!existsSync(filePath)) {
       throw new NotFoundException('File not found');
@@ -31,6 +27,29 @@ export class ImageService {
     return new StreamableFile(file, {
       disposition: `attachment; filename="${filename}"`,
     });
+  }
+
+  deleteImage(filename: string) {
+    const filePath = this.getFilePath(filename);
+
+    if (!existsSync(filePath)) {
+      throw new NotFoundException('File not found');
+    }
+
+    unlinkSync(filePath);
+  }
+
+  /* 
+    Get the file path for a given filename using cwd, UPLOAD_DIR (default to uploads) and filename
+    @param filename - The filename to get the path for
+    @returns The file path
+  */
+  private getFilePath(filename: string) {
+    return join(
+      process.cwd(),
+      process.env.UPLOAD_DIR || 'uploads',
+      filename,
+    );
   }
 
   private isSafeFilename(filename: string): boolean {
