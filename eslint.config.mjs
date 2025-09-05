80550c8abc60f2aadcaa42574c433c4820c2c433
Arthur2479 <arthurjacobs1004@gmail.com>
2025-09-05 15:55:38 +0200

    Configure prettier and eslint to ignore build, github and other unneeded folders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', '.github/**', 'dist/**', 'coverage/**', 'node_modules/**', 'letsencrypt/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
